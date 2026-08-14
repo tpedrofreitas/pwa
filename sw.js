5695bda27a0453f3f4ef8634e17c828efa97360a
Thiago Freitas <thifreitas26@gmail.com>
2026-08-14 20:49:27 -0300

    Codigo do Professor

--- sw.js
+++ sw.js
@@ -1,54 +1,51 @@
+const cacheName = 'ola-pwa-v2';
 
-
-const cacheName = "ola-pwa-v1";
-
-var filesToCache = [
+const filesToCache = [
     './',
-    './manifest.webmanifest',
     './index.html',
+    './manifest.webmanifest',
     './css/style.css',
     './js/main.js'
-]
+];
 
+// Instala o Service Worker e cria o cache
 self.addEventListener('install', event => {
+
     event.waitUntil(
         caches.open(cacheName)
             .then(cache => {
                 return cache.addAll(filesToCache);
             })
     );
+
     self.skipWaiting();
 });
 
+// Assume o controle das páginas abertas
 self.addEventListener('activate', event => {
+
     event.waitUntil(
         self.clients.claim()
     );
+
 });
 
+// Intercepta as requisições
 self.addEventListener('fetch', event => {
 
     event.respondWith(
         caches.match(event.request)
             .then(response => {
 
-
+                // Encontrou no cache
                 if (response) {
                     return response;
                 }
 
-
+                // Não encontrou → tenta a internet
                 return fetch(event.request);
 
             })
     );
 
 });
-
-
-
-
-
-
-
-
