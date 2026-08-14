1176d282b2864b2f735526ec2e99180d0b47e920
Thiago Freitas <thifreitas26@gmail.com>
2026-08-14 20:06:25 -0300

    Implementação dos eventos do Service Workes

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const { cache } = require("react");
+
 const cacheName = "ola-pwa";
 
 var filesToCache = [
@@ -7,3 +9,29 @@ var filesToCache = [
     './css/style.css',
     './js/main.js'
 ]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache =>
+                cache.addAll(filesToCache)
+            )
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response ||
+                    fetch(event.request);
+            }
+
+            )
+    );
+});
+
+
+
+
+
